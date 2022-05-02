c95668d7f7850d34d328bc3efefba6d2c8a33668
ckx000 <ckx000@gmail.com>
2022-05-02 18:18:25 +0800

    v1.0.2 小改进
    同时增加对旧版平台bug的兼容

--- js/cookie.js
+++ js/cookie.js
@@ -9,11 +9,15 @@ function onload(func) {
 onload(function() {
     console.log("getcookieload");
     document.body.setAttribute("get-cookie", "true");
-    window.postMessage({"info":"get-cookieModReady"}, "*");//预传递，在签到平台登录主页时自动填入info信息
-    window.addEventListener('click', function(event) {
+    window.postMessage({"info":"get-cookieModReady"}, "*");//预传递，测试自动填入功能
+            //添加旧版平台bug修复性兼容//将来弃置
+    if(location.pathname.indexOf('/edit')>0 ){$("a.ng-scope[data-toggle=get-cookie]").each(function(){this.removeAttribute("disabled")})}
+
+    window.addEventListener('click', function(event) {      
         var export_btn = event.target;
-        var port = chrome.runtime.connect({name: "get_cookie"});
         if (export_btn.getAttribute("data-toggle") == "get-cookie") {
+            var port = chrome.runtime.connect({name: "get_cookie"});
+            
             var site = export_btn.getAttribute("data-site");
             var name = export_btn.getAttribute("data-name");
             var domain = export_btn.getAttribute("data-domain");
@@ -21,8 +25,8 @@ onload(function() {
             port.postMessage({"do": "get_cookie", "site": site, "name": name, "domain": domain});
             port.onMessage.addListener(function(msg) {
                 if (window.confirm('你确定要此网站获取你' + site + '的Cookies么？')) {
-                    export_btn.setAttribute("data-cookie", JSON.stringify(msg));
-                    //console.log(msg);
+                    //export_btn.setAttribute("data-cookie", JSON.stringify(msg));
+                    console.log("postMessage",msg);
                     window.postMessage(msg, "*");//传递给页面消息，让网页填上cookies数据
                 } else {
                     return false;
