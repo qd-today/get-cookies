ab5bcbc2f21fd75c477474a96a665b517f4a830a
ckx000 <5800591+ckx000@users.noreply.github.com>
2026-04-20 14:44:31 +0800

    修改监听方式
    移除FF不兼容的hasListeners方法，改用全局注册监听

--- service_worker.js
+++ service_worker.js
@@ -20,50 +20,52 @@ chrome.storage.onChanged.addListener(function(changes, areaName){
         //console.log("true");
     }
 });
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {//tab页面刷新事件
+//全局注册监听
+chrome.runtime.onConnect.addListener((port) => {
+  handlePort(port);
+});
+function handlePort(port) {
+    console.assert(port.name == "get_cookie");
+    console.log("onMessageAddListener:",port);
+    port.onMessage.addListener(function(request) {
+        console.log(request);
+        if (request.do == "get_cookie") {
+            //console.log(request);
+            var option1 = {},option2 = {},tempurl;
+            if (request.site) {
+                option1["url"] = request.site;
+                //处理成域名来查找第2次
+                tempurl=new URL(request.site);
+                option2["domain"] = tempurl.hostname;
+            }
+            console.log(option1,option2);
+            const obj = {};
+            const getCookies = (options) => new Promise(resolve => {
+                chrome.cookies.getAll(options, resolve);
+            });
+            Promise.all([getCookies(option1), getCookies(option2)])
+                .then(([cookies1, cookies2]) => {
+                    const processCookie = cookie => obj[cookie.name] = cookie.value;
+                    cookies1.forEach(processCookie);
+                    cookies2.forEach(processCookie);
+                    const result = Object.keys(obj).length === 0 
+                        ? { error: "cookies值为空，请检查是否已登陆" }
+                        : obj;
+                    port.postMessage(result);
+                    console.log(result);
+                })
+                .catch(error => {
+                    console.error('Cookie获取失败:', error);
+                    port.postMessage({ error: "获取cookies时发生错误" });
+                });
+        }
+    });
+};
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {//tab页面刷新注入脚本
     //console.log(tab);
     if (changeInfo.status === 'loading' && urlcheck(tab.url)) {
         console.log("tab:",tab);
         chrome.scripting.executeScript({target: {tabId: tab.id},files: ["js/cookie.js"]});
-        if (!chrome.runtime.onConnect.hasListeners()) {
-            chrome.runtime.onConnect.addListener(function(port) {
-                console.assert(port.name == "get_cookie");
-                port.onMessage.addListener(function(request) {
-                    console.log(request);
-                    if (request.do == "get_cookie") {
-                        //console.log(request);
-                        var option1 = {},option2 = {},tempurl;
-                        if (request.site) {
-                            option1["url"] = request.site;
-                            //处理成域名来查找第2次
-                            tempurl=new URL(request.site);
-                            option2["domain"] = tempurl.hostname;
-                        }
-                        console.log(option1,option2);
-                        const obj = {};
-                        const getCookies = (options) => new Promise(resolve => {
-                            chrome.cookies.getAll(options, resolve);
-                        });
-                        Promise.all([getCookies(option1), getCookies(option2)])
-                            .then(([cookies1, cookies2]) => {
-                                const processCookie = cookie => obj[cookie.name] = cookie.value;
-                                cookies1.forEach(processCookie);
-                                cookies2.forEach(processCookie);
-                                const result = Object.keys(obj).length === 0 
-                                    ? { error: "cookies值为空，请检查是否已登陆" }
-                                    : obj;
-                                port.postMessage(result);
-                                console.log(result);
-                            })
-                            .catch(error => {
-                                console.error('Cookie获取失败:', error);
-                                port.postMessage({ error: "获取cookies时发生错误" });
-                            });
-                    }
-                });
-            });
-        }
-
     }
 
 });
